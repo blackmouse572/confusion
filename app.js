1410e223fbf5d9f3ad7a593402dcc4aa4e9d0016
blackmouse572 <ngocvlqt1995@gmail.com>
2023-06-16 17:35:18 +0700

    ass_3

---
+++ app.js
@@ -0,0 +1,86 @@
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const session = require("express-session");
+const FileStore = require("session-file-store")(session);
+const passport = require("passport");
+const authenticate = require("./authenticate");
+const config = require("./config");
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+const dishRouter = require("./routes/dishRouter");
+const promoRouter = require("./routes/promoRouter");
+const leaderRouter = require("./routes/leaderRouter");
+const mongoose = require("mongoose");
+
+const url = config.mongoUrl;
+const connect = mongoose.connect(url);
+
+const app = express();
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
+app.use(
+  session({
+    name: "session-id",
+    secret: "12345-67890",
+    saveUninitialized: false,
+    resave: false,
+    store: new FileStore(),
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+function auth(req, res, next) {
+  if (!req.user) {
+    var err = new Error("You are not authenticated!");
+    err.status = 403;
+    next(err);
+    return;
+  } else {
+    next();
+  }
+}
+app.use(auth);
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/dishes", dishRouter);
+app.use("/promotions", promoRouter);
+app.use("/leaders", leaderRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+connect.then(
+  (db) => {
+    console.log("Connected to mongo server at " + url);
+  },
+  (err) => {
+    console.log(err);
+  }
+);
+
+module.exports = app;
---
+++ authenticate.js
@@ -0,0 +1,63 @@
+var passport = require("passport");
+var LocalStrategy = require("passport-local").Strategy;
+var User = require("./models/user");
+var JwtStrategy = require("passport-jwt").Strategy;
+var ExtractJwt = require("passport-jwt").ExtractJwt;
+var jwt = require("jsonwebtoken"); // used to create, sign, and verify tokens
+
+var config = require("./config.js");
+const user = require("./models/user");
+
+exports.local = passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+exports.getToken = function (user) {
+  return jwt.sign(user, config.secretKey, {
+    expiresIn: 3600,
+  });
+};
+
+var opts = {};
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = config.jwtKey;
+
+exports.jwtPassport = passport.use(
+  new JwtStrategy(opts, (jwt_payload, done) => {
+    console.log("JWT payload: ", jwt_payload);
+
+    User.findOne({ _id: jwt_payload._id })
+      .then((user) => {
+        if (user) {
+          return done(null, user);
+        } else {
+          return done(null, false);
+        }
+      })
+      .catch((err) => done(err, false));
+  })
+);
+
+exports.verifyUser = passport.authenticate("jwt", { session: false });
+exports.verifyAdmin = (req, res, next) => {
+  if (req.user.admin) {
+    next();
+  } else {
+    const error = new Error(
+      "You are not authorized to perform this operation !"
+    );
+    error.status = 403;
+    return next(error);
+  }
+};
+exports.verifyAuthor = (req, res, next) => {
+  if (req.user._id.equals(req.params.userId)) {
+    next();
+  } else {
+    const error = new Error(
+      "You are not authorized to perform this operation !"
+    );
+    error.status = 403;
+    return next(error);
+  }
+};
